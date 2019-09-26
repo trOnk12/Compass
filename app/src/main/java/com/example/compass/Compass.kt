733bf284648d629f3185f213342f9a0c0ac4848e
trOnk12 <m.pachulski94@gmail.com>
2019-09-26 23:08:12 +0200

    basic rotation animation working

--- app/src/main/java/com/example/compass/Compass.kt
+++
@@ -1,5 +0,0 @@
-package com.example.compass
-
-class Compass(context : Context, attrs:AttributeSet) : LinearLayout(context,attrs) {
-
-}
---
+++ app/src/main/java/com/example/compass/CompassView.kt
@@ -0,0 +1,37 @@
+package com.example.compass
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.compass_view.view.*
+
+class CompassView(context: Context,attrs:AttributeSet) : LinearLayout(context,attrs) {
+    init {
+        View.inflate(context, R.layout.compass_view, this)
+    }
+
+    var currentDegree = 0f
+
+    fun rotateCompass(degree: Float) {
+
+        val rotateAnimation = RotateAnimation(
+            currentDegree,
+            -degree,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        ).apply {
+            duration = 2000
+            fillAfter = true
+        }
+
+        compass_image.startAnimation(rotateAnimation)
+
+        currentDegree = -degree
+    }
+
+}
