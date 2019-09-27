e765862cf26e6a980d6c400841bdc9cc8d2eb544
trOnk12 <m.pachulski94@gmail.com>
2019-09-27 11:41:10 +0200

    added koin dependencies on sensormanager,sensor

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -16,15 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        sensorService.getSensorData().subscribe {
-            for (value in it) {
-                Log.d("TEST", value.toString())
-            }
-        }
-
     }
-
 
 }
--- app/src/main/java/com/example/compass/MyApp.kt
+++ app/src/main/java/com/example/compass/MyApp.kt
@@ -17,5 +17,4 @@ class MyApp : Application() {
         }
     }
 
-
 }
